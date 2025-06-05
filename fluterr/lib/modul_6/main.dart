92da32ca7f10c2697f709b4cb49f5b2581c55cf1
FiTzZ6 <fikriramdan666@gmail.com>
2025-06-05 10:10:43 +0700

    fluter

---
+++ fluterr/lib/modul_6/main.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+import 'package:fluterr/modul_6/bottomnav.dart';
+import 'package:fluterr/modul_6/form.dart';
+import 'package:fluterr/modul_6/tabcontroller.dart';
+
+void main() {
+  runApp(
+    // BotApp()
+    // TabBarDemo()
+    Forms()
+    );
+}
